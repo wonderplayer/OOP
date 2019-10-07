11cb5025455b19078a7a38abf63662e907ffc58e
Rolands Strakis <roland.strakis@gmail.com>
2019-10-07 14:50:23 +0300

    LD5. Palaists uz Android Studio!

---
+++ LD5/chapter02/BeerAdviser/app/src/main/java/com/hfad/beeradviser/BeerExpert.java
@@ -0,0 +1,20 @@
+package com.hfad.beeradviser;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeerExpert {
+    List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<String>();
+        if (color.equals("amber")) {
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        } else if(color.equals("light")) {
+            brands.add("Gaišinieks");
+        }
+        else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
