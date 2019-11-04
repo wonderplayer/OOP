1dc89b261c161f340facea6bb501771825527d13
Rolands Strakis <roland.strakis@gmail.com>
2019-11-04 15:52:24 +0200

    Clojure projekts

--- LD9/Starbuzz/app/src/main/java/com/hfad/starbuzz/PiegadesCategoryActivity.java
+++ LD9/Starbuzz/app/src/main/java/com/hfad/starbuzz/PiegadesCategoryActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class PiegadesCategoryActivity extends Activity {
-    public static final String TEKSTS = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    public static final String TEKSTS = "Adrese: Miķeļa iela 1, Rīga.\nZvaniet pa telefonu 256354781.\nPiegādes darba laiks \n P-P: 10:00-18:00 \n Se-Sv: Nepiegādā";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
